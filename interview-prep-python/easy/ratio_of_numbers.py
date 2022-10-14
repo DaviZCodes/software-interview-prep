8cd6333aa5befef0ac173fbd75a5a9a93dbe7c5e
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2022-10-13 23:29:44 -0400

    Update ratio_of_numbers.py

--- interview-prep-python/easy/ratio_of_numbers.py
+++ interview-prep-python/easy/ratio_of_numbers.py
@@ -1,7 +1,7 @@
 '''
 File name
 Author: DaviZCodes
-Question from HackerRank:
+Question from HackerRank (easy):
 Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
 Print the decimal value of each fraction on a new line with  places after the decimal.
 Note: This challenge introduces precision problems.
