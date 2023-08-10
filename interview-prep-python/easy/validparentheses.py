e181736c33b70b57bedf6ebc732e2f2c4736fa2d
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2023-08-10 14:57:24 -0400

    Create validparentheses.py

---
+++ interview-prep-python/easy/validparentheses.py
@@ -0,0 +1,39 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+e.g 
+{{()}} is valid
+{[]) is invalid
+'''
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for char in s:
+            if char == "(" or char == "{" or char == "[":
+                stack.append(char)
+            else:
+                if len(stack) == 0:
+                    return False
+
+                if char == ")" and stack[-1] == "(":
+                    stack.pop()
+                elif char == "}" and stack[-1] == "{":
+                    stack.pop()
+                elif char == "]" and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+        
+        return len(stack) == 0
