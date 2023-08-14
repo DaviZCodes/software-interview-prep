1fe7bf50d7290ea194f1d521e25205d53402fa9c
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2023-08-14 15:24:35 -0400

    Create MinimumRotatedSortedArray.py

---
+++ interview-prep-python/medium/MinimumRotatedSortedArray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        min_result = nums[0] # initial value is first
+
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            if nums[left] <= nums[right]:
+                min_result = min(min_result, nums[left])
+                break
+            
+            mid = (left + right)//2
+            min_result = min(min_result, nums[mid])
+
+            if nums[mid] >= nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return min_result
