730cb03ec547b0813d1a604fdaaa27bfe112eca5
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2024-02-21 00:21:53 -0500

    Create LastStoneWeight.py

---
+++ interview-prep-python/easy/LastStoneWeight.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            return stones[0]
+        
+        # we negate everything because heapq.heapify() creates a min heap
+        stones = [-s for s in stones] 
+        heapq.heapify(stones)
+        print(stones)
+
+        while len(stones) > 1:
+            first, second = heapq.heappop(stones), heapq.heappop(stones)
+            #e.g. -6, -2
+
+            if first < second: # there could be a tie
+                heapq.heappush(stones, -(-first - (-second)))
+        
+        if len(stones) > 0:
+            return -stones[0]
+        else:
+            return 0 
+        
