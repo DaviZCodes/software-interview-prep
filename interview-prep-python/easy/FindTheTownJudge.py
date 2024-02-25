85211ab6ee197bf4bf924edb068d1a9f7f241564
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2024-02-25 16:21:37 -0500

    Update FindTheTownJudge.py

--- interview-prep-python/easy/FindTheTownJudge.py
+++ interview-prep-python/easy/FindTheTownJudge.py
@@ -1,4 +1,20 @@
+# this is a better soltuion since trust[i][0] might be confusing, default dicts are easier to handle with
 class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        incoming = defaultdict(int) # we use default dict to avoid out of bounds error
+        outgoing = defaultdict(int) # initialized at zero, must be integers
+
+        for src, dst in trust: # each element is like [a, b]
+            incoming[dst] += 1
+            outgoing[src] += 1
+        
+        for i in range(1, n + 1):
+            if incoming[i] == (n - 1) and outgoing[i] == 0: # everyone trusts the judge but themselves so (n - 1)
+                return i
+        
+        return -1 # no town judge found
+        
+'''class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         in_degree = [0] * (n + 1) # in_degree means someone trusts you
         out_degree = [0] * (n + 1) # out degree means you trust someone
@@ -14,5 +30,6 @@ class Solution:
                 return i
         
         return -1
+'''
         
         
