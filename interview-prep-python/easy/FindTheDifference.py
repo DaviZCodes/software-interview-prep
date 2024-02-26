d375053cc24030b970c1c2bccb0aad81ea968a2b
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2024-02-25 21:17:16 -0500

    Update and rename FindTheDifference to FindTheDifference.py

--- interview-prep-python/easy/FindTheDifference.py
+++ interview-prep-python/easy/FindTheDifference.py
@@ -1,8 +1,21 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        # best solution
+        sum_s = 0
+        sum_t = 0
+
+        for char in s:
+            sum_s += ord(char)
+        
+        for char in t:
+            sum_t += ord(char)
+        
+        return chr(sum_t - sum_s)
+
+        ''' another solution
         count_char1 = Counter(s)
         count_char2 = Counter(t)
-        '''
+        
         count_char1 = {}
         count_char2 = {}
 
@@ -16,10 +29,11 @@ class Solution:
             if char not in count_char2:
                 count_char2[char] = 1
             else:
-                count_char2[char] += 1'''
+                count_char2[char] += 1
         
         for char in count_char2:
             if char not in count_char1:
                 return char
             if count_char1[char] != count_char2[char]:
                 return char 
+        '''
